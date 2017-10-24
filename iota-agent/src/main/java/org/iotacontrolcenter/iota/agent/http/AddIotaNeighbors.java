bc0cc32723d13c6a80692480fd969a70eb31d886
bahamapascal <bahamapascal@yahoo.com>
2017-10-24 03:00:24 +0200

    Update AddIotaNeighbors.java

--- iota-agent/src/main/java/org/iotacontrolcenter/iota/agent/http/AddIotaNeighbors.java
+++ iota-agent/src/main/java/org/iotacontrolcenter/iota/agent/http/AddIotaNeighbors.java
@@ -5,5 +5,6 @@ public class AddIotaNeighbors extends HttpPost {
     public AddIotaNeighbors(String url) {
         super("addIotaNeighbors", url);
         addHeader("Content-Type", "application/json");
+        addHeader("X-IOTA-API-Version", "1.4.1");
     }
 }
