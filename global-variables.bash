d83b6ac114e2b0e8a3843431198f72b36b1372c7
ahab <abdshah94@gmail.com>
2018-01-11 14:14:44 +0500

    fix iccr container

--- global-variables.bash
+++ global-variables.bash
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 #Script to initiate common variables that are used globally.
 
@@ -7,6 +8,6 @@ group=$(id -Gn $user | awk '{print $1}')
 dir=$PWD/dist/temp
 iccrdir=$dir/iccr
 dist=$PWD/dist
-ICC_PROD_DIR=~/projects/extjs/iccw/build/production/icc
+API_KEY="!API_KEY"
+export user group dir iccrdir dist version API_KEY
 
-export user group dir iccrdir dist version ICC_PROD_DIR
