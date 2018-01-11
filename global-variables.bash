3fb82765252e7d08f97c43ccbdf50c59d43b386d
ahab <abdshah94@gmail.com>
2018-01-11 14:16:38 +0500

    add iccr prop dir var in glob

--- global-variables.bash
+++ global-variables.bash
@@ -9,5 +9,6 @@ dir=$PWD/dist/temp
 iccrdir=$dir/iccr
 dist=$PWD/dist
 API_KEY="!API_KEY"
-export user group dir iccrdir dist version API_KEY
+ICC_PROD_DIR=~/projects/extjs/iccw/build/production/icc
+export user group dir iccrdir dist version API_KEY ICC_PROD_DIR
 
