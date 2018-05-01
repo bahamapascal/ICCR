a9ce007fb67ce851a0c27ba5c991d85c1daf8924
ahab94 <abdshah94@gmail.com>
2018-05-01 13:08:52 +0500

    add variable support for cron jobs

--- installer/iccr-scripts/src/main/scripts/startiota.bash
+++ installer/iccr-scripts/src/main/scripts/startiota.bash
@@ -9,15 +9,18 @@ iotaDir=`grep iotaDir $iccrDir/conf/iccr.properties | sed -e 's/iotaDir=//g'`
 iotaStartCmd=`grep iotaStartCmd $iccrDir/conf/iccr.properties | sed -e 's/iotaStartCmd=//g'`
 iotaPortNumber=`grep iotaPortNumber $iccrDir/conf/iccr.properties | sed -e 's/iotaPortNumber=//g'`
 iotaPidFile=$iotaDir/iota.pid
+iotaOnBoot=`grep startIotaOnBoot $iccrDir/conf/iccr.properties | sed -e 's/startIotaOnBoot=//g'`
 
 cd $iotaDir
 startCmd="nohup ${iotaStartCmd} ${iotaPortNumber} > console.log 2>&1 &"
 
-echo "Creating cron job"
-if  crontab -l | grep -q "$startCmd"; then
-	echo "crontab already exists... Ignoring"
-else
-       	crontab -l | { cat; echo "@reboot $startCmd"; } | crontab -
+if [ "$iotaOnBoot" = true ] ; then
+    echo "Creating cron job"
+    if  crontab -l | grep -q "$startCmd"; then
+	    echo "crontab already exists... Ignoring"
+    else
+        	crontab -l | { cat; echo "@reboot $startCmd"; } | crontab -
+    fi
 fi
 
 echo "${startCmd}"
