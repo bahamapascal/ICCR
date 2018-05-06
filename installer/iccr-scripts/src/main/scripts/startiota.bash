8981866ae99b2e34930c2aacf866670f7560e025
ahab94 <abdshah94@gmail.com>
2018-05-06 22:53:27 +0500

    enhance script crons

--- installer/iccr-scripts/src/main/scripts/startiota.bash
+++ installer/iccr-scripts/src/main/scripts/startiota.bash
@@ -9,21 +9,8 @@ iotaDir=`grep iotaDir $iccrDir/conf/iccr.properties | sed -e 's/iotaDir=//g'`
 iotaStartCmd=`grep iotaStartCmd $iccrDir/conf/iccr.properties | sed -e 's/iotaStartCmd=//g'`
 iotaPortNumber=`grep iotaPortNumber $iccrDir/conf/iccr.properties | sed -e 's/iotaPortNumber=//g'`
 iotaPidFile=$iotaDir/iota.pid
-iotaOnBoot=`grep startIotaOnBoot $iccrDir/conf/iccr.properties | sed -e 's/startIotaOnBoot=//g'`
 
 cd $iotaDir
-
-
-if [ "$iotaOnBoot" = true ] ; then
-    cronJob="cd $iotaDir && nohup ${iotaStartCmd} ${iotaPortNumber} > console.log 2>&1"
-    echo "Creating cron job"
-    if  crontab -l | grep -q "$cronJob"; then
-	    echo "crontab already exists... Ignoring"
-    else
-        crontab -l | { cat; echo "@reboot $cronJob"; } | crontab -
-    fi
-fi
-
 
 nohup ${iotaStartCmd} ${iotaPortNumber}  > console.log 2>&1 &
 
